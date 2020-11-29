edc0b5d5961c6f338fcae32c2113e939b9f16ba7
kanamea <kanamekanamea@gmail.com>
2020-11-29 02:23:57 -0800

    added loading screen

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -7,6 +7,14 @@ const HTML_FILE = path.join(DIST_DIR, 'index.html')
 
 app.use(express.static(DIST_DIR))
 
+app.use((req, res, next) => {
+    if (req.secure) {
+        next()
+    } else {
+        res.redirect('https://' + req.headers.host + req.url)
+    }
+})
+
 app.get('*', (req, res) => {
     res.sendFile(HTML_FILE)
 })
