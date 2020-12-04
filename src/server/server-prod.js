daca1f16e74b96b3df7644ad55bab98de0611a19
kanamea <kanamekanamea@gmail.com>
2020-12-04 13:54:44 -0800

    update server

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -5,15 +5,35 @@ const app = express()
 const DIST_DIR = __dirname
 const HTML_FILE = path.join(DIST_DIR, 'index.html')
 
-app.use(express.static(DIST_DIR))
-
 app.use((req, res, next) => {
+    // if "http"
     if (req.headers['x-forwarded-proto'] !== 'https') {
-        return next()
+        var split = req.headers.host.split(".")
+        
+        // format: www.pekora-march.com
+        if (split.length === 3 && split[0] === "www") {
+            return res.redirect('https://' + req.headers.host + req.url)
+        } else if (split.length === 2) {
+            return res.redirect('https://www.' + req.headers.host + req.url)
+        // any other format, just return as-is
+        } else {
+            next()
+        }
+    // if "https"
     } else {
-        return res.redirect('https://' + req.headers.host + req.url)
+        var split = req.headers.host.split(".")
+        
+        // format: pekora-march.com
+        if (split.length === 2) {
+            return res.redirect('https://www.' + req.headers.host + req.url)
+        // any other format, just return as-is
+        } else {
+            next()
+        }
     }
 })
+
+app.use(express.static(DIST_DIR))
 
 app.get('*', (req, res) => {
     res.sendFile(HTML_FILE)
